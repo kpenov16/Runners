3b77c16aea25089e4b14c69bd86541a2c32e461b
Armandas <armandas.rokas@gmail.com>
2019-03-25 14:42:51 +0100

    Moved to config

--- src/main/java/dk/runs/runners/RunApplication.java
+++ src/main/java/dk/runs/runners/RunApplication.java
@@ -1,16 +1,18 @@
 package dk.runs.runners;
 
 import dk.runs.runners.repositories.RunRepositoryImpl;
+import dk.runs.runners.runsconfig.RunsConfig;
 import dk.runs.runners.services.run.RunService;
 import dk.runs.runners.services.run.RunServiceImpl;
 import dk.runs.runners.usecases.RunRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+//@Configuration
 @SpringBootApplication
 public class RunApplication {
     public static void main(String[] args) {
@@ -18,19 +20,22 @@ public class RunApplication {
         ctx.register(RunsConfig.class);
         ctx.refresh();*/
         SpringApplication.run(RunApplication.class, args);
+
+     //   AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+      //          RunsConfig.class);
     }
 
-    //@Bean(name="runRepository")
-    @Bean
-    @Scope("prototype")
-    public static RunRepository getRunRepository(){
-        return new RunRepositoryImpl();
-    }
-
-    //@Bean(name="runService")
-    @Bean
-    @Scope("prototype")
-    public static RunService getRunService(){
-        return new RunServiceImpl(getRunRepository());
-    }
+//    //@Bean(name="runRepository")
+//    @Bean
+//    @Scope("prototype")
+//    public static RunRepository getRunRepository(){
+//        return new RunRepositoryImpl();
+//    }
+//
+//    //@Bean(name="runService")
+//    @Bean
+//    @Scope("prototype")
+//    public static RunService getRunService(){
+//        return new RunServiceImpl(getRunRepository());
+//    }
 }
--- src/main/java/dk/runs/runners/runsconfig/RunsConfig.java
+++ src/main/java/dk/runs/runners/runsconfig/RunsConfig.java
@@ -1,23 +1,44 @@
 package dk.runs.runners.runsconfig;
 
 import dk.runs.runners.repositories.RunRepositoryImpl;
+import dk.runs.runners.services.run.RunService;
 import dk.runs.runners.services.run.RunServiceImpl;
+import dk.runs.runners.usecases.RunRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+
+@ComponentScan("dk.runs.runners")
 @Configuration
 public abstract class RunsConfig {
 
-    @Bean(name="runRepository")
+//    @Bean(name="runRepository")
+//    @Scope("prototype")
+//    public static RunRepositoryImpl getRunRepository(){
+//        return new RunRepositoryImpl();
+//    }
+//
+//    @Bean(name="runService")
+//    @Scope("prototype")
+//    public static RunServiceImpl getRunService(){
+//        return new RunServiceImpl(getRunRepository());
+//    }
+
+    //@Bean(name="runRepository")
+    @Bean
     @Scope("prototype")
-    public static RunRepositoryImpl getRunRepository(){
+    public RunRepository getRunRepository(){
         return new RunRepositoryImpl();
     }
 
-    @Bean(name="runService")
+    //@Bean(name="runService")
+    @Bean
     @Scope("prototype")
-    public static RunServiceImpl getRunService(){
+    public RunService getRunService(){
         return new RunServiceImpl(getRunRepository());
     }
+
+
 }
