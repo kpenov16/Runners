6997045a75748be8975eb01fdfa660c66238c380
Armandas <armandas.rokas@gmail.com>
2019-03-26 12:38:50 +0100

    added givenCreateRunWithExistingId_returnRunServiceException test to RunControllerTest

--- src/test/java/dk/runs/runners/rest/RunControllerImplTest.java
+++ src/test/java/dk/runs/runners/rest/RunControllerImplTest.java
@@ -23,10 +23,29 @@ class RunControllerImplTest {
     }
 
     @Test
-    void givenGetRuns_expectedException_returnNull() {
-      //  when(runService.getRunsList()).thenThrow(new RunService.RunServiceException(""));
+    void givenCreateRunWithExistingId_returnRunServiceException() {
+        // Arrange
+        RunRequest runRequest = new RunRequest();
+        Run run = new Run();
+        runRequest.setRun(run);
+        runRequest.setCreatorId("");
 
+        doThrow(new RunService.RunServiceException("")).when(runService).createRun(runRequest.getRun(), runRequest.getCreatorId()); //  have to be same object as in  controller addRun call
+
+        // Act & Assert
+        assertThrows(RunService.RunServiceException.class,
+                () -> controller.addRun(runRequest)
+        );
+    }
+
+
+
+    @Test
+    void givenGetRuns_expectedException_returnNull() {
+
+        // Arrange
         doThrow(new RunService.RunServiceException("")).when(runService).getRunsList();
+        // Act & Assert
         assertThrows(RunService.RunServiceException.class,
                 () -> controller.getRuns()
         );
@@ -34,13 +53,17 @@ class RunControllerImplTest {
 
     @Test
     void givenGetRuns_returnEmptyList() {
+        // Arrange
         when(runService.getRunsList()).thenReturn(new LinkedList<Run>());
+        // Act & Assert
         assertEquals(new LinkedList<Run>(), controller.getRuns());
     }
 
     @Test
     void givenIdToDelete_InvokeDeleteMethodInRunService(){
+        // Act
         controller.deleteRun("0000");
+        // Assert
         verify(runService).deleteRun("0000");
     }
 }
