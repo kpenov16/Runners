50b17a0675a2ae23f6886625135b9aa3408a36ca
Armandas <armandas.rokas@gmail.com>
2019-04-07 17:28:25 +0200

    added ddl_waypoint and added Foreign key for creator_id

---
+++ docs/sql/DDL_waypoints.sql
@@ -0,0 +1,10 @@
+/**********************************************
+Creating the table
+**********************************************/
+CREATE TABLE IF NOT EXISTS `waypoint`(
+	`index` INT,
+	`route_id` VARCHAR(100),
+    `spatial_point` POINT NOT NULL SRID 0,
+    PRIMARY KEY (`index`, `route_id`),
+    FOREIGN KEY (`route_id`) REFERENCES route(`id`)
+);
