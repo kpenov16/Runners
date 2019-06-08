99c14bc040b573a652536c15a0436e6db54d922e
FilipBLJensen <filblj@gmail.com>
2019-06-08 10:18:33 +0200

    Added DataSource class and HikariCP maven dependency

---
+++ src/main/java/dk/runs/runners/datasourceconfig/DataSource.java
@@ -0,0 +1,29 @@
+package dk.runs.runners.datasourceconfig;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource dataSource;
+
+    static {
+        config.setJdbcUrl("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s133967?");
+        config.setUsername("s133967");
+        config.setPassword("8JPOJuQcgUpUVIVHY4S2H");
+        config.addDataSourceProperty("cachePrepStmts","true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    private DataSource(){}
+
+    public static Connection getConnection()throws SQLException {
+        return dataSource.getConnection();
+    }
+}
