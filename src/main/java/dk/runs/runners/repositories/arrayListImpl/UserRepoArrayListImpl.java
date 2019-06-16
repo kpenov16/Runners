b9504acd26836ce3be39d3e8dd3a98be17478125
Armandas <armandas.rokas@gmail.com>
2019-06-16 19:10:24 +0200

    updateUser fixes

--- src/main/java/dk/runs/runners/repositories/arrayListImpl/UserRepoArrayListImpl.java
+++ src/main/java/dk/runs/runners/repositories/arrayListImpl/UserRepoArrayListImpl.java
@@ -1,23 +1,29 @@
 package dk.runs.runners.repositories.arrayListImpl;
 
+import dk.runs.runners.entities.Location;
 import dk.runs.runners.entities.User;
 import dk.runs.runners.services.interfaceRepositories.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class UserRepoArrayListImpl implements UserRepository {
 
     private static List<User> users = new ArrayList<>();
 
     static {
-        users.add(new User("1", "John", "john@email.com", "123", null, null));
-        users.add(new User("2", "Tom", "tom@email.com", "123", null, null));
-        users.add(new User("3", "Jan", "jan@email.com", "123", null, null));
+        users.add(new User("1", "John", "john@email.com", "123", null,
+                new ArrayList<Location>(){{add(new Location(UUID.randomUUID().toString(), 0, 2,2, "New York", "USA", "Main street", "55C"));}}));
+        users.add(new User("2", "Tom", "tom@email.com", "123", null,
+                new ArrayList<Location>(){{add(new Location(UUID.randomUUID().toString(), 0, 2,2, "Copenhagen", "Denmark", "Hovedgaden", "1"));}}));
+        users.add(new User("3", "Jan", "jan@email.com", "123", null,
+                new ArrayList<Location>(){{add(new Location(UUID.randomUUID().toString(), 0, 2,2, "Vilnius", "Lithuania", "Gedemino pr.", "99"));}}));
     }
 
     @Override
     public void createUser(User user) throws UserIdDuplicationException, UserNameDuplicationException, UserEmailDuplicationException, CreateUserException, UserMissingLocationException {
+        validateUser(user);
         for(User u: users){
             if(u.getUserName().equalsIgnoreCase(user.getUserName())){
                 throw new UserNameDuplicationException("User name duplication");
@@ -31,6 +37,17 @@ public class UserRepoArrayListImpl implements UserRepository {
         }
 
         users.add(user);
+    }
+    private void validateUser(User user) {
+        if(user.getLocations() == null || user.getLocations().size() == 0){
+            throw new UserMissingLocationException("User with id: " + user.getId() + " is missing location.");
+        }
+
+        for(Location location : user.getLocations()){
+            if(location.getId() == null || location.getId().isEmpty()){
+                throw new UserMissingLocationException("User with id: " + user.getId() + " is missing location.");
+            }
+        }
     }
 
     @Override
@@ -66,6 +83,19 @@ public class UserRepoArrayListImpl implements UserRepository {
 
     @Override
     public void updateUser(User updatedUser) throws UserNameDuplicationException, UserEmailDuplicationException, UpdateUserException, UserNotFoundException {
+        int indexOfUpdateUser = 0;
+
+        for(User u: users){
+            if(u.getId().equalsIgnoreCase(updatedUser.getId())){
+                indexOfUpdateUser = users.indexOf(u);
+            }
+        }
+
+        if(indexOfUpdateUser == 0){
+            throw new UserNotFoundException("User not found");
+        }
+
+
         User userToRemove = null;
         for(User u: users){
             if(u.getUserName().equalsIgnoreCase(updatedUser.getUserName())){
